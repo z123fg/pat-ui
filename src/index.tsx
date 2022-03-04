730dbe39136bebf5464fcb4a11ae54527b1bb303
yizhou zhou <yizhouzhou@yizhous-MacBook-Pro.local>
2022-03-04 11:38:22 -0600

    finish layout and props

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Switch, { SwitchSizeType } from './components/Switch/Switch';
 export { default as Button } from './components/Button';
 export { default as Input } from './components/Input';
 export { default as Icon } from './components/Icon';
@@ -5,3 +9,19 @@ export { default as Message } from './components/Message';
 export { default as Card } from './components/Card';
 export { default as Dropdown } from './components/Dropdown';
 export { default as Progress } from './components/Progress';
+
+
+function App() {
+    return (
+      <div className='App' style={{margin:'50px'}}>
+        <Switch label={'Hello World'} size={SwitchSizeType.Small}/>
+      </div>
+    );
+  }
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
