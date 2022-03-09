1dea6f2f48615b835fb214cd90ad25a60df3b175
louis09392003 <louis09392003@gmail.com>
2022-03-09 01:01:06 -0700

    try select without dropdown

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { FC } from 'react';
 import Select, { ISelectProps } from './Select';
 // import DropdownOption, { IDropdownOptionProps } from './DropdownOption';
@@ -11,7 +10,4 @@ export type PatSelectComponent = FC<ISelectProps>
 const TransSelect = Select as PatSelectComponent;
 // TransDropdown.Option = DropdownOption;
 
-export default TransSelect;
-=======
-export { default } from './Select';
->>>>>>> 3cd3f0141e90e7146ac3f1b51d1dd7ab80ecb78c
+export default TransSelect
